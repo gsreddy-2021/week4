7956b67b11b6346e06f0174d751c52fbcc167b7f
S Reddy <sachitgreddy@email.com>
2021-01-28 21:29:58 -0500

    almost fixed the code

--- lab/week4.js
+++ lab/week4.js
@@ -18,6 +18,39 @@ async function pageLoaded() {
 
   // writes the returned JSON to the console
   console.log(json)
+
+  // 1. get the data you are working with
+  let products = json.products
+  console.log(products)
+
+  // 2. write code to loop through the products
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i]
+
+    // 3. render each product: make it work first
+    // console.log(product)
+
+    let element = document.querySelector('.products')
+    // console.log(element)
+
+    // 4. Render product
+    console.log(product)
+    element.insertAdjacentHTML('beforeend', `
+    <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+        <div class="border h-full p-4 flex flex-col">
+          <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+          <div class="mb-4"><img src="${product.image}">
+          </div>
+          <div class="mb-4 text-gray-900">
+            ${product.description}
+          </div>
+          <div class="mt-auto text-purple-500 text-2xl">$${product.price}</div>
+        </div>
+      </div>
+    `)
+
+  }
+
   
   // 🔥 start here: write code to loop through the products; each
   // iteration of the loop should call your renderProduct function
